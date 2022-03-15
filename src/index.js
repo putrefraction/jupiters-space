f4c8cfe5b521fea8426f609be3d46e0f6924c36f
JupiterIsis <jupiteryzing@gmail.com>
2022-03-15 18:06:29 -0300

    quick session experiment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const session = require("express-session")
+const bodyParser = require("body-parser")
 const route = require("./route")
 const path = require("path")
 
@@ -12,5 +14,13 @@ server.use(express.static("public"))
 server.use(express.urlencoded({"extended":true}))
 
 server.use(route)
+
+server.use(session({
+    secret:"thisisasecret",
+    saveUninitialized: false,
+    resave: false
+}))
+
+server.use(bodyParser.json())
 
 server.listen('3000', () => console.log("RUNNING AT PORT 3000"))
--- src/route.js
+++ src/route.js
@@ -4,7 +4,12 @@ const route = express.Router()
 const PostController = require('./controllers/PostController')
 
 
-route.get('/', (req, res) => {res.render("main", {page: "home"})})
+route.get('/', (req, res) => {
+    sess = req.session
+    if(sess.email)
+
+    res.render("main", {page: "home"})
+})
 
 route.get('/blog', PostController.index)
 route.get('/blog/:date/:slug', PostController.open)
