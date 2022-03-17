c09380cb5b186834f0c904fe442b8204bfb53fe9
JupiterIsis <jupiteryzing@gmail.com>
2022-03-16 21:46:03 -0300

    Add new post interface

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -5,9 +5,7 @@ const Database = require("../db/config")
 module.exports = {
     // Selects and renders a specific post from the database, based on its slug
     async open(req, res){
-        const db = await Database()
-        const session = req.session ? req.session : false;
-        
+        const db = await Database()     
 
         const postSlug = req.params.slug
         const postDate = req.params.date
@@ -21,7 +19,7 @@ module.exports = {
         const body = postContents.body
         
         if (date == postDate && postSlug == urlSlug){
-            res.render("main", {page:"post",title: title, subtitle: subtitle, date:date, body:body, session:session})
+            res.render("main", {page:"post",title: title, subtitle: subtitle, date:date, body:body, session:req.session})
         } else {
             res.render("not-found")
         }
@@ -31,12 +29,54 @@ module.exports = {
     // Selects all posts from the database in order to create an index view at /blog
     async index(req, res){
         const db = await Database()
-        const session = req.session ? req.session : false;
 
         const posts = await db.all("SELECT * FROM posts")
         const isPosts = posts.length > 0
 
-        res.render("main", {page:"post-index", posts: posts, isPosts: isPosts, session:session})
+        res.render("main", {page:"post-index", posts: posts, isPosts: isPosts, session:req.session})
+
+    },
+
+    async new(req, res){
+        const db = await Database()
+
+        function slugify(str){
+            str = str.replace(/^\s+|\s+$/g, '');
+
+            // Make the string lowercase
+            str = str.toLowerCase();
+
+            // Remove accents, swap ñ for n, etc
+            var from = "ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆa·/_,:;";
+            var to   = "AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa------";
+            for (var i=0, l=from.length ; i<l ; i++) {
+                str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+            }
+
+            // Remove invalid chars
+            str = str.replace(/[^a-z0-9 -]/g, '') 
+            // Collapse whitespace and replace by -
+            .replace(/\s+/g, '-') 
+            // Collapse dashes
+            .replace(/-+/g, '-'); 
+
+            return str;
+        }
+
+        const postTitle = req.body.post_title
+        const postSlug = slugify(postTitle)
+        const postSubtitle = req.body.post_subtitle
+        const postBody = req.body.post_body
+
+        await db.exec(`INSERT INTO posts(title, urlSlug, subtitle, date, body) VALUES (
+            "${postTitle}",
+            "${postSlug}",
+            "${postSubtitle}",
+            datetime("now","localtime"),
+            "${postBody}"
+        )`)
+
+        res.redirect("/blog")
 
     }
 }
--- src/route.js
+++ src/route.js
@@ -6,13 +6,15 @@ const SessionController = require('./controllers/SessionController')
 
 
 
-route.get('/', (req, res) => {res.render("main", {page: "home"})})
+route.get('/', (req, res) => {res.render("main", {page: "home", session:req.session})})
 
 route.get('/blog', PostController.index)
 route.get('/blog/:date/:slug', PostController.open)
 
+route.get('/new-post', (req, res) => {res.render("main", {page: "new-post", session:req.session})})
+route.post('/new-post/submit', PostController.new)
+
 route.get('/admin', (req,res) => {res.render("login")})
 route.post('/admin/login', SessionController.login)
-
 
 module.exports = route
